4a8dae50980bbf9e27d8dff53e97a4737d60ac70
raulglezrdguez <raulglezrguez@gmail.com>
2022-01-30 18:15:12 -0500

    material ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import Stack from '@mui/material/Stack';
 
-function App() {
-  return <div className="App">App works</div>;
-}
+import Typography from '@mui/material/Typography';
+
+const App = () => {
+  return (
+    <Stack spacing={2} alignItems={'center'}>
+      <Typography variant="h2" component="div" gutterBottom>
+        Tasks
+      </Typography>
+    </Stack>
+  );
+};
 
 export default App;
