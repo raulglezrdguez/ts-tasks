2bc4b3e2f848cc5b75ccda2c910947c6dd30147b
raulglezrdguez <raulglezrguez@gmail.com>
2022-01-30 18:24:10 -0500

    add task form created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Stack from '@mui/material/Stack';
 
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
@@ -10,6 +13,15 @@ const App = () => {
       <Typography variant="h2" component="div" gutterBottom>
         Tasks
       </Typography>
+      <Stack spacing={2} direction="row">
+        <TextField
+          id="newTask"
+          label="New Task"
+          placeholder="Enter new task"
+          variant="outlined"
+        />
+        <Button variant="contained">Add</Button>
+      </Stack>
     </Stack>
   );
 };
